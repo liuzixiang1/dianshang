f899ea1cd6c349bc0be0583501505eb0a0d14164
liuzixiang <643239214@qq.com>
2021-08-16 22:46:10 +0800

    更新

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -86,7 +86,7 @@
             ></el-table-column>
             <el-table-column label="操作">
               <template slot-scope="">
-                <el-button size="mini" type="primary" icon="el-icon-edit"
+                <el-button size="mini" type="primary" icon="el-icon-edit" @click="showEditDialog"
                   >编辑</el-button
                 >
                 <el-button size="mini" type="danger" icon="el-icon-delete"
@@ -103,7 +103,7 @@
       :title="'添加' + titleText"
       :visible.sync="addDialogVisible"
       width="50%"
-      @close="addDialog"
+      @close="addDialogClosed"
     >
       <!-- 添加参数的对话框 -->
       <el-form
@@ -118,7 +118,32 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false"
+        <el-button type="primary" @click="addParams"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+    <!-- 修改参数的对话框 -->
+        <el-dialog
+      :title="'修改' + titleText"
+      :visible.sync="editDialogVisible"
+      width="50%"
+      @close="editDialogClosed"
+    >
+      <!-- 添加参数的对话框 -->
+      <el-form
+        :model="editForm"
+        :rules="editFormRules"
+        ref="editFormRef"
+        label-width="100px"
+      >
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="editForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editParams"
           >确 定</el-button
         >
       </span>
@@ -157,7 +182,15 @@ export default {
       // 添加表单的验证规则对象
       addFormRules:{
         attr_name:[{required:true,message:'请输入参数名称',trigger:'blur'}]
-      }
+      },
+      // 控制修改对话框的显示与隐藏
+      editDialogVisible:false,
+      // 修改的表单数据对象
+      editForm:{},
+      // 修改表单的验证规则对象
+      editFormRules:{
+        attr_name:[{required:true,message:'请输入参数名称',trigger:'blur'}]
+      },
     };
   },
   created() {
@@ -204,6 +237,33 @@ export default {
         this.onlyTableData = res.data;
       }
     },
+    // 监听对话框的关闭事件
+    addDialogClosed(){
+      this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮，添加参数
+    addParams(){
+      this.$refs.addFormRef.validate(async valid => {
+        if(!valid) return
+        const{data:res} = await this.$axios.post(`categories/${this.cateId}/attributes`,{attr_name:this.addForm.attr_name,attr_sel:this.activeName})
+        if(res.meta.status !== 201){
+        return this.$message.error('添加参数失败!')
+      }
+     })
+      this.$message.success('添加参数成功!')
+      this.addDialogVisible = false
+      this.getParamsData()
+    },
+    // 点击按钮，展示修改的对话框
+    showEditDialog(){
+      this.editDialogVisible = true
+    },
+    // 重置修改的表单
+    editDialogClosed(){
+      this.$refs.editFormRef.resetFields()
+    },
+    // 点击按钮，修改参数信息
+    editParams(){}
   },
   computed: {
     // 如果按钮需要被禁用，则返回true，否则返回false
