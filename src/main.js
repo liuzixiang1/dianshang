c5d24e93f434a8414768983a1da891ee94bc83d7
刘子翔 <643239214@qq.com>
2021-08-17 13:54:27 +0800

    商品列表

--- src/main.js
+++ src/main.js
@@ -24,6 +24,20 @@ axios.interceptors.request.use(config => {
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 Vue.prototype.$axios = axios
 
+Vue.filter('dateFormat',function(originVal)
+{
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2,'0')
+
+  const hh = (dt.getHours() + '').padStart(2,'0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 Vue.component('tree-table',TreeTable)
 
 // Vue实例
