c69c4299543849e97bdd7fc6d135d1cec59912e7
刘子翔 <643239214@qq.com>
2021-08-17 16:38:03 +0800

    完成了报表功能的开发

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,20 +1,107 @@
 <template>
   <div>
-      订单列表  
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row></el-row>
+      <!-- 订单列表数据 -->
+      <el-table :data="orderlist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type="success" v-if="scope.row.pay_status === '1'"
+              >已付款</el-tag
+            >
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send">
+          <template slot-scope="scope">
+            {{ scope.row.is_send }}
+          </template>
+        </el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">
+            {{ scope.row.create_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <el-button
+              type="success"
+              icon="el-icon-location"
+              size="mini"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      >
+      </el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{}
+  data() {
+    return {
+      queryInfo: {
+        query: "",
+        pagenum: 1,
+        pagesize: 10,
+      },
+      total: 0,
+      orderlist: [],
+    };
+  },
+  created() {
+    this.getOrderList();
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$axios.get("orders", {
+        params: this.queryInfo,
+      });
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取订单列表失败!");
+      }
+      console.log(res);
+      this.total = res.data.total;
+      this.orderlist = res.data.goods;
     },
-    created(){},
-    methods:{}
-
-}
+    handleSizeChange(newSize){
+        this.queryInfo.pagesize = newSize
+        this.getOrderList()
+    },
+    handleCurrentChange(newPage){
+        this.queryInfo.pagenum = newPage
+        this.getOrderList()
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
